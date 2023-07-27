b28eb9f285ad625f8a379124bbf43913c1cddbca
mariareeves <mariapaula.zimmers@gmail.com>
2023-07-27 12:52:48 -0600

    add adopt button

--- server/server.js
+++ server/server.js
@@ -4,8 +4,20 @@ const path = require('path')
 
 app.use(express.static('public'))
 
+
+// include and initialize the rollbar library with your access token
+var Rollbar = require('rollbar')
+var rollbar = new Rollbar({
+    accessToken: 'b2a4ea2f10784bb8bea86b11b29da5dc',
+    captureUncaught: true,
+    captureUnhandledRejections: true,
+})
+
+// record a generic message and send it to Rollbar
+rollbar.log('Hello world!')
+
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '../public/html'))
+    res.sendFile(path.join(__dirname, '../public/index.html'))
 })
 
 app.listen(4000, () => {
