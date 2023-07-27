94f68a7be0f7cd61a59bc9a94b7f6cd1ab086530
mariareeves <mariapaula.zimmers@gmail.com>
2023-07-27 12:57:10 -0600

    add alert to the adopt button

---
+++ public/main.js
@@ -0,0 +1,9 @@
+console.log('test')
+
+const adoptBtn = document.getElementById('adopt-btn')
+
+function adopt() {
+    alert('Thank you for supporting all the puppies. We love you!')
+}
+
+adoptBtn.addEventListener('click', adopt)
